7a19573723ac48c558f652ffe42c814dedc0fc6e
Maksudul Haque <e01716@gmail.com>
2023-02-11 02:30:57 +0600

    Login page added

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,1 +1,90 @@
-<template>Login View</template>
+<template>
+  <div class="container">
+    <div class="login-form">
+      <div class="form-detail">
+        <h2>Login</h2>
+        <br />
+        <form action="submit">
+          <div class="login-credential">
+            <div>
+              <input
+                type="text"
+                name=""
+                id=""
+                class="user"
+                placeholder="Enter your email address"
+              />
+            </div>
+            <div class="password-and-account">
+              <input
+                type="password"
+                name=""
+                id=""
+                class="password"
+                placeholder="Enter your password"
+              />
+              <br />
+              <a href="">Forgot password</a>
+            </div>
+          </div>
+          <br />
+          <br />
+          <button>Sign In</button>
+          <br />
+          <a href="">Create Account</a>
+        </form>
+      </div>
+    </div>
+    <div class="footer">
+      <FooterComponent />
+    </div>
+  </div>
+</template>
+
+<script setup>
+import FooterComponent from "@/components/FooterComponent.vue";
+</script>
+
+<style scoped>
+.container {
+  min-width: 100%;
+  height: 100%;
+}
+
+input {
+  padding: 8px;
+  width: 306px;
+}
+
+h2 {
+  font-weight: 400;
+  font-size: 42px;
+}
+
+.login-credential {
+  display: flex;
+}
+
+button {
+  all: unset;
+  border: 1px solid black;
+  width: 200px;
+  height: 48px;
+  text-align: center;
+  cursor: pointer;
+}
+.user {
+  margin-right: 24px;
+}
+.login-form {
+  /* border: 1px solid red; */
+  display: grid;
+  place-items: center;
+  height: 50vh;
+}
+
+.form-detail {
+  /* border: 1px solid yellow; */
+  width: fit-content;
+}
+</style>
